e641014738e0bb0a6cad265c03dd245bddea2cb9
Maverick0008 <93142563+Maverick0008@users.noreply.github.com>
2022-02-19 20:27:28 +0300

    init

---
+++ 85.js
@@ -0,0 +1,23 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Billie Joe",
+      "title" : "Vocal",
+      "release_year": 2001,
+      "formats" : [
+        "CD",
+        "LP"
+      ]
+    }
+    
+  ];
