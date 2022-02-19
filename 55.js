df8e474e03d669c63357dbd5bf1ed13f63cad5a7
Maverick0008 <93142563+Maverick0008@users.noreply.github.com>
2022-02-19 15:38:03 +0300

    init

---
+++ 55.js
@@ -0,0 +1,10 @@
+// Setup
+function compareEquality(a, b) {
+    if (typeof a === typeof b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  console.log(compareEquality(10, "10"));
+  
