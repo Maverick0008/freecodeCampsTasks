e52961dba482ec5ebd0d9c3b362ce0b6f0e42ea1
Maverick0008 <93142563+Maverick0008@users.noreply.github.com>
2022-02-19 17:17:05 +0300

    init

---
+++ 76.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Boby",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Mark", "Bark"]
+  
+    // Only change code above this line
+  };
